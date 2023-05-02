41c9d34e4ccb657030cd1b028eb441827e869aa9
my <jakha921@gmail.com>
2023-05-02 15:48:58 +0500

    routers to login pages

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Request
+from fastapi_users import FastAPIUsers
+from fastapi_users.authentication import AuthenticationBackend, BearerTransport, CookieTransport, JWTStrategy
 
-app = FastAPI()
+from src.auth.database import User
+from src.auth.jwt import auth_backend
+from src.auth.manager import get_user_manager
+from src.auth.schemas import UserRead, UserCreate
+
+app = FastAPI(
+    title="API for Todo List",
+    description="This is a very fancy project, with auto docs for the API and everything",
+    version="0.0.1",
+)
 
 
 @app.get("/")
@@ -8,6 +19,61 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+# ----------------------
+
+bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
+cookie_transport = CookieTransport(cookie_max_age=3600)
+
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+
+
+jwt_backend = AuthenticationBackend(
+    name="jwt",
+    transport=bearer_transport,
+    get_strategy=get_jwt_strategy,
+)
+cookie_backend = AuthenticationBackend(
+    name="jwt",
+    transport=cookie_transport,
+    get_strategy=get_jwt_strategy,
+)
+
+
+async def get_enabled_backends(request: Request):
+    """Return the enabled dependencies following custom logic."""
+    if request.url.path == "/protected-route-only-jwt":
+        return [jwt_backend]
+    else:
+        return [cookie_backend, jwt_backend]
+
+
+current_active_user = fastapi_users.current_user(active=True, get_enabled_backends=get_enabled_backends)
+
+
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_active_user)):
+    return f"Hello, {user.email}"
+
+
+@app.get("/unprotected-route")
+def protected_route():
+    return f"Hello, anonymous user"
